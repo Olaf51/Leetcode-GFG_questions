d25732dc7c92d0e9168b973f6054089d7436edc8
Olaf51 <112055781+Olaf51@users.noreply.github.com>
2023-07-12 03:29:46 +0530

    Added solution - LeetHub

--- Detect cycle in a directed graph - GFG/detect-cycle-in-a-directed-graph.cpp
+++ Detect cycle in a directed graph - GFG/detect-cycle-in-a-directed-graph.cpp
@@ -6,26 +6,47 @@ using namespace std;
 class Solution {
   public:
     // Function to detect cycle in a directed graph.
-    bool dfs(int node,vector<int>&vis,vector<int>&path,vector<int> adj[]){
-        vis[node] = 1;
-        path[node] = 1;
-        for(auto it: adj[node]){
-            if(!vis[it]){
-                if(dfs(it,vis,path,adj) == true)return true;
-            }
-            else if(path[it]!=0)return true;
-        }
-        path[node] = 0;
-        return  false;
-    }
+    // bool dfs(int node,vector<int>&vis,vector<int>&path,vector<int> adj[]){
+    //     vis[node] = 1;
+    //     path[node] = 1;
+    //     for(auto it: adj[node]){
+    //         if(!vis[it]){
+    //             if(dfs(it,vis,path,adj) == true)return true;
+    //         }
+    //         else if(path[it]!=0)return true;
+    //     }
+    //     path[node] = 0;
+    //     return  false;
+    // }
     bool isCyclic(int V, vector<int> adj[]) {
         // code here
-        vector<int>vis(V,0);
-        vector<int>path(V,0);
+        // vector<int>vis(V,0);
+        // vector<int>path(V,0);
+        // for(int i=0;i<V;i++){
+        //     if(dfs(i,vis,path,adj)==true)return true;
+        // }
+        // return false;
+        vector<int>ind(V,0);
         for(int i=0;i<V;i++){
-            if(dfs(i,vis,path,adj)==true)return true;
+            for(auto it: adj[i])
+            ind[it]++;
         }
-        return false;
+        queue<int>qt;
+        for(int i=0;i<V;i++){
+            if(ind[i] == 0)qt.push(i);
+        }
+        int cnt = 0;
+        while(!qt.empty()){
+            int temp = qt.front();
+            qt.pop();
+            cnt++;
+            for(auto it: adj[temp]){
+                ind[it]--;
+                if(ind[it] == 0)qt.push(it);
+            }
+        }
+        if(cnt == V)return false;
+         return true;
     }
 };
 
