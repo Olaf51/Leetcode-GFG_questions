a37d19055ca80a63f0781ac949a8a865cb754a8a
Olaf51 <112055781+Olaf51@users.noreply.github.com>
2023-07-31 11:11:01 +0530

    Time: 125 ms (31.96%) | Memory: 9.4 MB (41.87%) - LeetSync

--- 664-strange-printer/strange-printer.cpp
+++ 664-strange-printer/strange-printer.cpp
@@ -1,25 +1,19 @@
 class Solution {
 public:
-    int n;
-    vector<vector<int>> dp;
-    int f(int i, int j, string& s){       
-        if (dp[i][j]!=-1) return dp[i][j];
-        if (i==j) return dp[i][j]=1;
-        int ans;
-        if (s[i]==s[j]||s[j-1]==s[j]) ans=f(i, j-1, s);
-        else if (s[i]==s[i+1]) ans=f(i+1, j, s);
-        else{
-            ans=f(i,j-1, s)+1;
-            for(int k=i+1; k<j; k++){
-                if(s[k]==s[j])
-                    ans=min(ans, f(i, k-1, s)+f(k, j-1, s));
-            }
-        }
-        return dp[i][j]=ans;
+    int solve(const string& s,int i,int j,vector<vector<int>>&dp){
+        if(i==j)return 1;
+        if(dp[i][j]!=-1)return dp[i][j];
+        int ans = INT_MAX;
+        for(int k=i;k<j;k++)
+        ans = min(ans, solve(s,i,k,dp)+solve(s,k+1,j,dp));
+        if(s[i]==s[j])
+        return dp[i][j] = ans-1;
+        else
+        return dp[i][j] = ans;
     }
     int strangePrinter(string s) {
-        n=s.size();
-        dp.assign(n, vector<int>(n, -1));
-        return f(0, n-1, s);
+      int n = s.size();
+      vector<vector<int>>dp(n,vector<int>(n,-1));
+      return solve(s,0,n-1,dp); 
     }
 };
