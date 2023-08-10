7c7618bd0ca823138152941c290f33d2689e06d7
Olaf51 <112055781+Olaf51@users.noreply.github.com>
2023-08-10 20:00:27 +0530

    Time: 3 ms (74.13%) | Memory: 11.2 MB (15.78%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -3,33 +3,50 @@ public:
     int search(vector<int>& nums, int target) {
         int n = nums.size();
         int s = 0,e= n-1;
-        int piv = 0;
+        // int piv = 0;
+        // while(s<=e){
+        // int m = s + (e-s)/2;
+        // if(nums[m]>=nums[0])
+        // s = m+1;
+        // else
+        // piv = m, e = m-1;
+        // }
+        // s =0,e=n-1;
+        // int t = piv;
+        // while(s<=t){
+        //     int m =s + (t-s)/2;
+        //     if(nums[m] == target)
+        //     return m;
+        //     else if(target<nums[m])
+        //     t = m-1;
+        //     else
+        //     s = m+1;
+        // }
+        // while(piv<=e){
+        //     int m =piv + (e-piv)/2;
+        //     if(nums[m] == target)
+        //     return m;
+        //     else if(target<nums[m])
+        //     e = m-1;
+        //     else
+        //     piv = m+1;
+        // }
         while(s<=e){
-        int m = s + (e-s)/2;
-        if(nums[m]>=nums[0])
-        s = m+1;
-        else
-        piv = m, e = m-1;
-        }
-        s =0,e=n-1;
-        int t = piv;
-        while(s<=t){
-            int m =s + (t-s)/2;
-            if(nums[m] == target)
-            return m;
-            else if(target<nums[m])
-            t = m-1;
-            else
-            s = m+1;
-        }
-        while(piv<=e){
-            int m =piv + (e-piv)/2;
-            if(nums[m] == target)
-            return m;
-            else if(target<nums[m])
-            e = m-1;
-            else
-            piv = m+1;
+            int mid = s + (e-s)/2;
+            if(nums[mid] == target)return mid;
+            if(nums[s] <=nums[mid]){
+                if(nums[s]<=target && target <= nums[mid])
+                e = mid-1;
+                else
+                s = mid+1;
+
+            }
+            else{
+                if(nums[mid]<=target && target <= nums[e])
+                s = mid+1;
+                else
+                e = mid-1;
+            }
         }
         return -1;
     }
