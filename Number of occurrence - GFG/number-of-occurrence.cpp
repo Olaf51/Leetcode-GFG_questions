e19a7e7e23ae894140fc1d397d4c2bfd46663d7f
Olaf51 <112055781+Olaf51@users.noreply.github.com>
2023-08-20 19:20:44 +0530

    Added solution - LeetHub

--- Number of occurrence - GFG/number-of-occurrence.cpp
+++ Number of occurrence - GFG/number-of-occurrence.cpp
@@ -10,36 +10,12 @@ class Solution{
 public:	
 	/* if x is present in arr[] then returns the count
 		of occurrences of x, otherwise returns 0. */
-	int count(int nums[], int n, int x) {
+	int count(int arr[], int n, int x) {
 	    // code here
-	      int s = 0,e = n-1;
-        int f=-1,l=-1;
-        while(s<=e){
-            int mid = s + (e-s)/2;
-            if(x == nums[mid]){
-                f = mid;
-                e = mid-1;
-            }
-            else if(x< nums[mid])
-            e = mid-1;
-            else
-            s = mid+1;
-        }
-        s = 0, e = n-1;
-         while(s<=e){
-            int mid = s + (e-s)/2;
-            if(x == nums[mid]){
-                l = mid;
-                s = mid+1;
-            }
-            else if(x< nums[mid])
-            e = mid-1;
-            else
-            s = mid+1;
-        }
-        if(f!=-1 && l!= -1)
-        return l-f + 1;
-        return 0;
+	    unordered_map<int,int>mp;
+	 for(int i=0;i<n;i++)
+	 mp[arr[i]]++;
+	    return mp[x];
 	}
 };
 
