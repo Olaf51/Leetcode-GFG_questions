54ff3994b1e96047a8145cb9e5b72bcee99c8b19
Olaf51 <112055781+Olaf51@users.noreply.github.com>
2023-09-13 08:02:37 +0530

    Time: 55 ms (63.03%) | Memory: 17.2 MB (92.64%) - LeetSync

--- 1770-minimum-deletions-to-make-character-frequencies-unique/minimum-deletions-to-make-character-frequencies-unique.cpp
+++ 1770-minimum-deletions-to-make-character-frequencies-unique/minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
-     int minDeletions(string s) {
-        unordered_map<char, int> chars;
-        for (char c : s) {
-            chars[c]++;
-        }
+    int minDeletions(string s) {
+        vector<int>v(26,0);
+        for(auto i: s)
+        v[i-'a']++;
 
-        unordered_set<int> freqSet;
-        int count = 0;
-
-        for (const auto& pair : chars) {
-            int freq = pair.second;
-            while (freq > 0 && freqSet.count(freq)) {
-                freq--;
-                count++;
+        int del = 0;
+        sort(v.begin(),v.end());
+        for(int i=24;i>=0;i--){
+            if(v[i]==0)
+            break;
+            if(v[i]>=v[i+1]){
+                int prev = v[i];
+                v[i] = max(0,v[i+1]-1);
+                del += prev-v[i];
             }
-            freqSet.insert(freq);
         }
-
-        return count;        
+        return del;
+        
     }
 };
