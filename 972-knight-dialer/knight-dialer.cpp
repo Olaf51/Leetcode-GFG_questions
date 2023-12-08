8100634a40a93d31ab422f305a0abdcd7c2af4ec
Olaf51 <112055781+Olaf51@users.noreply.github.com>
2023-12-08 22:57:41 +0530

    Time: 64 ms (74.20%) | Memory: 8.2 MB (79.38%) - LeetSync

--- 972-knight-dialer/knight-dialer.cpp
+++ 972-knight-dialer/knight-dialer.cpp
@@ -1,37 +1,33 @@
-#pragma GCC optimize("O3")
 class Solution {
 public:
-    const int mod=1e9+7;
-    vector<vector<int>> adj={
-        {4, 6},{6, 8},{7, 9},{4, 8}, {3, 9, 0},
-        {}, {1, 7, 0},{2, 6}, {3, 1},{2,4}
-    };
-   
-    vector<vector<int>> dp;
-    int f(int i, int n){
-        if (n==1) return 1;
-        if (i==5) return 0;
-        if (dp[i][n]!=-1) return dp[i][n];
-        int ans=0;
-        #pragma unroll
-        for(int j: adj[i]){
-            ans=(ans+f(j, n-1))%mod;
-        }
-        return dp[i][n]=ans;
+int mod = 1e9 + 7;
+vector<vector<int>>adj = {
+    {4,6},
+    {6,8},
+    {7,9},
+    {4,8},
+    {3,0,9},
+    {},
+    {1,7,0},
+    {2,6},
+    {1,3},
+    {2,4}};
+    int dp[5000][10];
+    int solve(int n, int cell){
+        if(n==0)return 1;
+        if(dp[n][cell]!= -1)return dp[n][cell];
+        int ans = 0;
+        for(auto c : adj[cell])
+        ans = (ans + solve(n-1,c))%mod;
+        return dp[n][cell] = ans;
     }
+
     int knightDialer(int n) {
-        int ans=0;
-        dp.assign(10, vector<int>(n+1, -1));
-        #pragma unroll
-        for(int i=0; i<=9; i++)
-            ans=(ans+f(i, n))%mod;
-        return ans;
+        memset(dp, -1, sizeof(dp));
+        int res = 0;
+        for(int cell = 0;cell<=9;cell++){
+            res  = (res + solve(n-1,cell))%mod;
+        }
+        return res;
     }
 };
-auto init = []()
-{ 
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-    return 'c';
-}();
