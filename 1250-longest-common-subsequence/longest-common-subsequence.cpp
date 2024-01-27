483a82ac3d3af54a723dc706b58778de6c1699cd
Olaf51 <112055781+Olaf51@users.noreply.github.com>
2024-01-27 12:41:25 +0530

    Time: 15 ms (91.73%) | Memory: 7.5 MB (93.57%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,29 +1,25 @@
 class Solution {
 public:
-    int solve(string &text1, string &text2, vector<vector<int>>&dp, int i, int j){
-        if(i<0 || j<0)return 0;
-        if(dp[i][j]!=-1)return dp[i][j];
-        if(text1[i] == text2[j])
-        return dp[i][j] = 1 + solve(text1,text2,dp, i-1, j-1);
-        return dp[i][j] = max(solve(text1,text2,dp,i-1,j),solve(text1, text2, dp, i, j-1));
-    }
     int longestCommonSubsequence(string text1, string text2) {
+        int t[1001][1001];
+        memset(t,-1,sizeof(t));
         int n = text1.size();
-        int m = text2.size();
-       // vector<vector<int>>dp(n+1, vector<int>(m+1,-1));
-       vector<int>prev(m+1,0);
-        vector<int>curr(m+1,0);
-        //return solve(text1, text2, dp, n-1,m-1);
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=m;j++){
+        int m =text2.size();
+       // for(int i=0;i<text1.size();i++)t[i][0] = 0;
+        //for(int i=0;i<text2.size();i++)t[0][i] = 0;
+        vector<int>curr(m+1,0),prev(m+1,0);
+        for(int i=1;i<=text1.size();i++){
+            for(int j =1;j<=text2.size();j++){
                 if(text1[i-1] == text2[j-1])
-              curr[j] = 1+ prev[j-1];
-        else
-         curr[j] = max(prev[j],curr[j-1]);
+                curr[j] = 1 + prev[j-1];
+                else
+                curr[j] = max(prev[j] , curr[j-1]);
             }
             prev = curr;
         }
-        return prev[m];
+      //  return t[text1.size()][text2.size()];
+      return prev[m];
+
         
     }
 };
