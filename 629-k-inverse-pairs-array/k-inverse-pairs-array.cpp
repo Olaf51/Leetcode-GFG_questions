345f325eeb77bbc001c7b297e897bdc46615326d
Olaf51 <112055781+Olaf51@users.noreply.github.com>
2024-01-27 20:08:22 +0530

    Time: 2483 ms (6.59%) | Memory: 11.6 MB (39.56%) - LeetSync

--- 629-k-inverse-pairs-array/k-inverse-pairs-array.cpp
+++ 629-k-inverse-pairs-array/k-inverse-pairs-array.cpp
@@ -2,17 +2,17 @@ class Solution {
 public:
     int t[1001][1001];
     int mod = 1e9 + 7;
-    int solve(int n, int k){
-        if(n==0)return 0;
-        if(k==0)return 1;
-        if(t[n][k]!=-1)return t[n][k];
-        int res = 0;
-        for(int i=0;i<=min(k,n-1);i++)
-        res = (res%mod + solve(n-1,k-i)%mod)%mod;
+    // int solve(int n, int k){
+    //     if(n==0)return 0;
+    //     if(k==0)return 1;
+    //     if(t[n][k]!=-1)return t[n][k];
+    //     int res = 0;
+    //     for(int i=0;i<=min(k,n-1);i++)
+    //     res = (res%mod + solve(n-1,k-i)%mod)%mod;
 
-        return t[n][k] = res;
+    //     return t[n][k] = res;
 
-    }
+    // }
     int kInversePairs(int n, int k) {
         memset(t,0,sizeof(t));
         for(int i=0;i<=n;i++)
