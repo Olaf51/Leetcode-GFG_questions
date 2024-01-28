1471a6677687474f48c0dae4686346c156dc8ba0
Olaf51 <112055781+Olaf51@users.noreply.github.com>
2024-01-28 14:13:23 +0530

    Time: 4 ms (29.98%) | Memory: 9 MB (65.09%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -11,8 +11,15 @@ int t[101];
   }
     int rob(vector<int>& nums) {
         int n = nums.size();
-        memset(t,-1,sizeof(t));
-        return solve(nums,n,0);
+      //  memset(t,-1,sizeof(t));
+       // return solve(nums,n,0);
+       t[0] = 0,t[1] = nums[0];
+       for(int i=2;i<=n;i++){
+           int steal = nums[i-1] + t[i-2];
+           int skip = t[i-1];
+           t[i] = max(steal,skip);
+       }
+       return t[n];
         
 
         
