990848face28a43fec588004cadd2854fff032b5
Olaf51 <112055781+Olaf51@users.noreply.github.com>
2024-01-30 19:01:44 +0530

    Time: 9 ms (55.73%) | Memory: 15.6 MB (5.26%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.cpp
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.cpp
@@ -9,13 +9,20 @@ public:
  }
     int evalRPN(vector<string>& tokens) {
         stack<int>st;
+        unordered_map<string,function<int(int,int)>>mp = {
+            {"+", [](int a, int b){return a+b;}},
+            {"-", [](int a, int b){return a-b;}},
+            {"*", [](int a, int b){return (long)a*(long)b;}},
+            {"/", [](int a, int b){return a/b;}}
+        };
+        
         for(auto &t: tokens){
             if(t=="+" || t=="-" || t=="*" || t=="/"){
                 int a = st.top();
                 st.pop();
                   int b = st.top();
                 st.pop();
-                int res = ope(b,a,t);
+                int res = mp[t](b,a);
                 st.push(res);
             }
             else
