953859efba41c4540c51f1ae8fe649f00be6b319
Olaf51 <112055781+Olaf51@users.noreply.github.com>
2024-02-20 23:14:03 +0530

    Time: 27 ms (18.16%) | Memory: 21 MB (10.05%) - LeetSync

--- 139-word-break/word-break.cpp
+++ 139-word-break/word-break.cpp
@@ -1,33 +1,26 @@
 class Solution {
 public:
-bool solve(string s,  unordered_set<string>&st, vector<int>&memo, int i){
-    if(i == s.size())return 1;
-    if(memo[i]!= -1)return memo[i];
-    for(int j=i;j<s.size();j++){
-        if(st.count(s.substr(i,j+1-i)) && solve(s, st, memo,j+1)){
-            memo[i] = true;
-            return true;
-
-        }
-    }
-    return memo[i] = false;
-}
+  int dp[301];
+  int n;
+  unordered_set<string>st;
+   bool solve(int idx, string & s){
+       if(idx == n)return true;
+       if(dp[idx]!=-1)return dp[idx];
+       if(st.find(s)!= st.end())return true;
+       for(int i=1;i<=n;i++){
+           string temp = s.substr(idx,i);
+           if(st.find(temp)!= st.end() && solve(idx+i,s))
+           return dp[idx] = true;
+       }
+       return dp[idx] = false;
+   }
     bool wordBreak(string s, vector<string>& wordDict) {
-        unordered_set<string>st(wordDict.begin(), wordDict.end());
-     
-       int m = s.size();
-        vector<int>memo(m+1,0);
-       // return solve(s, st, memo,0);
-        memo[0] = 1;
-        for(int i=1;i<=m;i++){
-           for(int j=0;j<i;j++){
-               if(memo[j] && st.count(s.substr(j,i-j))){
-                   memo[i] = 1;
-                   break;
-               }
-           }
-        }
-        return memo[m];
+        memset(dp,-1,sizeof(dp));
+        n = s.size();
+        for(auto it: wordDict)
+        st.insert(it);
+        return solve(0,s);
+
         
     }
 };
