3cb53fe474f3034ff3694d8d6826ecb7810a6cc1
Olaf51 <112055781+Olaf51@users.noreply.github.com>
2024-02-28 19:32:44 +0530

    Time: 7 ms (79.35%) | Memory: 20.3 MB (51.68%) - LeetSync

--- 513-find-bottom-left-tree-value/find-bottom-left-tree-value.cpp
+++ 513-find-bottom-left-tree-value/find-bottom-left-tree-value.cpp
@@ -11,20 +11,31 @@
  */
 class Solution {
 public:
-   int maxD = -1;
-   int res = 0;
-    void dfs(TreeNode* root, int curr){
-        if(!root)return;
+//    int maxD = -1;
+//    int res = 0;
+//     void dfs(TreeNode* root, int curr){
+//         if(!root)return;
 
-        if(curr > maxD){
-            maxD = curr;
-            res = root->val;
-        }
-        dfs(root->left,curr+1);
-        dfs(root->right, curr + 1);
-    }
+//         if(curr > maxD){
+//             maxD = curr;
+//             res = root->val;
+//         }
+//         dfs(root->left,curr+1);
+//         dfs(root->right, curr + 1);
+//     }
     int findBottomLeftValue(TreeNode* root) {
-        dfs(root,0);
+        // dfs(root,0);
+        // return res;
+        int res=0;
+        queue<TreeNode*>q;
+        q.push(root);
+        while(!q.empty()){
+         TreeNode* temp = q.front();
+         q.pop();
+         res = temp->val;
+         if(temp->right)q.push(temp->right);
+         if(temp->left)q.push(temp->left);
+        }
         return res;
         
     }
