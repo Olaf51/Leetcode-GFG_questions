3106199e50cb7403ea12097432649af82da3da3f
Olaf51 <112055781+Olaf51@users.noreply.github.com>
2024-03-13 17:01:02 +0530

    Time: 3 ms (49.05%) | Memory: 8.1 MB (45.01%) - LeetSync

--- 807-custom-sort-string/custom-sort-string.cpp
+++ 807-custom-sort-string/custom-sort-string.cpp
@@ -1,21 +1,31 @@
 class Solution {
 public:
     string customSortString(string order, string s) {
-        int cnt[26] = {0};
-        for(auto &ch : s)
-        cnt[ch-'a']++;
+        // int cnt[26] = {0};
+        // for(auto &ch : s)
+        // cnt[ch-'a']++;
 
-        string res = "";
-        for(auto &it: order){
-            while(cnt[it-'a']--)
-            res.push_back(it);
-        }
+        // string res = "";
+        // for(auto &it: order){
+        //     while(cnt[it-'a']--)
+        //     res.push_back(it);
+        // }
 
-        for(auto &ch : s){
-            if(cnt[ch-'a']>0){
-               res.push_back(ch);
-            }
+        // for(auto &ch : s){
+        //     if(cnt[ch-'a']>0){
+        //        res.push_back(ch);
+        //     }
+        // }
+        // return res;
+        vector<int>cnt(26,0);
+        for(int i=0;i<order.size();i++){
+            char ch = order[i];
+            cnt[ch-'a'] = i;
         }
-        return res;
+        auto myComp = [&cnt](char &ch1, char &ch2){
+            return cnt[ch1-'a'] < cnt[ch2-'a'];
+        };
+        sort(begin(s),end(s),myComp);
+        return s;
     }
 };
