adfa44f968f85d85a9af9a569ec25e51f0fd98b3
Olaf51 <112055781+Olaf51@users.noreply.github.com>
2024-03-15 23:35:13 +0530

    Time: 12 ms (83.78%) | Memory: 26.4 MB (72.09%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -1,39 +1,51 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        long long prod = 1;
-        int n = nums.size();
+        // long long prod = 1;
+        // int n = nums.size();
 
-        int cnt = 0,fz=-1;
-         for(int i=0;i<n;i++){
-            if(nums[i] == 0)
-            cnt++;
-            if(nums[i]!=0)
-            prod = prod*nums[i];
+        // int cnt = 0,fz=-1;
+        //  for(int i=0;i<n;i++){
+        //     if(nums[i] == 0)
+        //     cnt++;
+        //     if(nums[i]!=0)
+        //     prod = prod*nums[i];
+        // }
+        // if(cnt > 1){
+        //  vector<int>ans(n,0);
+        //  return ans;
+        //  }
+        // for(int i=0;i<n;i++){
+        //     if(nums[i]==0){
+        //         fz = i;
+        //         break;
+        //     }
+        // }
+        // if(cnt == 1){
+        //      for(int i=0;i<n;i++){
+        //             if(i==fz)
+        //              nums[fz] = prod;
+        //              else
+        //              nums[i] = 0;
+        //                  }
+        //     return nums;
+        // }
+        // for(int i=0;i<n;i++){
+        //     nums[i] = prod/nums[i];
+        // }
+        // return nums;
+        int n = nums.size();
+        vector<int>res(n);
+        res[0] = 1;
+        for(int i=1;i<n;i++)
+        res[i] = nums[i-1]*res[i-1];
+
+        int rprod=1;
+        for(int i=n-1;i>=0;i--){
+            res[i] = res[i]*rprod;
+            rprod = rprod*nums[i];
         }
-        if(cnt > 1){
-         vector<int>ans(n,0);
-         return ans;
-         }
-        for(int i=0;i<n;i++){
-            if(nums[i]==0){
-                fz = i;
-                break;
-            }
-        }
-        if(cnt == 1){
-             for(int i=0;i<n;i++){
-                    if(i==fz)
-                     nums[fz] = prod;
-                     else
-                     nums[i] = 0;
-                         }
-            return nums;
-        }
-        for(int i=0;i<n;i++){
-            nums[i] = prod/nums[i];
-        }
-        return nums;
+        return res;
 
 
        
