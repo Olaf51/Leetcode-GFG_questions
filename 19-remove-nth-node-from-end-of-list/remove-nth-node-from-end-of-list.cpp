7061309b3b7ae5d34c4b3dbd7cef679e3f90acc2
Olaf51 <112055781+Olaf51@users.noreply.github.com>
2024-05-29 18:49:45 +0530

    Time: 8 ms (6.81%) | Memory: 13.4 MB (19.70%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
@@ -8,51 +8,29 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        int length = 0;
-        ListNode* curr = head;
-        
-        // Calculate the length of the linked list
-        while (curr != nullptr) {
-            length++;
-            curr = curr->next;
+        if(!head)return NULL;
+        int l=0;
+        ListNode* temp = head;
+        while(temp){
+            l++;
+            temp = temp->next;
         }
-
-        // Find the position to remove
-        int traverseTill = length - n - 1;
-        int i = 0;
-        curr = head;
-
-        // If the head needs to be removed
-        if (traverseTill == -1) {
+        int rl = l-n;
+        if(rl == 0){
             head = head->next;
-            delete curr;
             return head;
         }
-
-        // Traverse to the node before the one to be removed
-        while (i < traverseTill) {
-            curr = curr->next;
-            i++;
+        temp = head;
+        while(rl>1){
+            rl--;
+          temp = temp->next;
         }
-
-        // Remove the nth node from the end
-        ListNode* temp = curr->next;
-        curr->next = curr->next->next;
-        delete temp;
-
+        temp->next = temp->next->next;
+       
         return head;
+        
     }
 };
