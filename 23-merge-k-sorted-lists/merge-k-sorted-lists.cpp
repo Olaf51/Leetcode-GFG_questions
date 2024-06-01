cada4168dc9f41333fc275063cefc11017694a32
Olaf51 <112055781+Olaf51@users.noreply.github.com>
2024-06-01 15:22:50 +0530

    Time: 18 ms (46.80%) | Memory: 17.2 MB (37.07%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
@@ -10,52 +10,30 @@
  */
 class Solution {
 public:
-    ListNode* merge(ListNode* head1, ListNode* head2){
-       
-        // ListNode* d = new ListNode(-1);
-        // ListNode* m = d;
-        // while(head1 && head2){
-        //     if(head1->val <=head2->val){
-        //         m->next = head1;
-        //         m = m->next;
-        //         head1 = head1->next;
-        //     }
-        //     else{
-        //         m->next = head2;
-        //         m = m->next;
-        //         head2 = head2->next;
-        //     }
-        // }
-        // if(head1)m->next = head1;
-        // if(head2)m->next = head2;
-        // return d->next;
-      if(!head1)return head2;
-        if(!head2)return head1;
-        if(head1->val <= head2->val){
-            head1->next = merge(head1->next, head2);
-            return head1;
-        }
-        else{
-            head2->next = merge(head1, head2->next);
-            return head2;
-        }
-        return 0;
-        
-    }
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-          if(lists.size() == 0){
+        if(lists.size() == 0){
             return NULL;
         }
         if(lists.size() == 1){
             return lists[0];
         }
-       ListNode* head = lists[0];
-       for(int i=1;i<lists.size();i++){
-        head =  merge(head,lists[i]);
-    
-       }
-      
+        priority_queue<pair<int,ListNode*>, vector<pair<int, ListNode*>>, greater<pair<int,ListNode*>>>pq;
+        for(int i=0;i<lists.size();i++)
+        if(lists[i])
+         pq.push({lists[i]->val,lists[i]});
 
-       return head;
+        ListNode* d = new ListNode(-1);
+        ListNode* temp = d;
+         while(!pq.empty()){
+           pair<int,ListNode*>p = pq.top();
+           pq.pop();
+           temp->next = p.second;
+           temp = temp->next;
+           if(p.second->next)
+           pq.push({p.second->next->val,p.second->next});
+         }
+         return d->next;
+        
+        
     }
 };
