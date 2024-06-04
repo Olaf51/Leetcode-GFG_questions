637650a9699c4992fbc77d04da68eecb0b3f8f5f
Olaf51 <112055781+Olaf51@users.noreply.github.com>
2024-06-04 16:22:37 +0530

    Time: 4 ms (41.02%) | Memory: 13.4 MB (77.53%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,52 +1,21 @@
-        class Solution {
+class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int n = nums.size();
-        int s = 0,e= n-1;
-        // int piv = 0;
-        // while(s<=e){
-        // int m = s + (e-s)/2;
-        // if(nums[m]>=nums[0])
-        // s = m+1;
-        // else
-        // piv = m, e = m-1;
-        // }
-        // s =0,e=n-1;
-        // int t = piv;
-        // while(s<=t){
-        //     int m =s + (t-s)/2;
-        //     if(nums[m] == target)
-        //     return m;
-        //     else if(target<nums[m])
-        //     t = m-1;
-        //     else
-        //     s = m+1;
-        // }
-        // while(piv<=e){
-        //     int m =piv + (e-piv)/2;
-        //     if(nums[m] == target)
-        //     return m;
-        //     else if(target<nums[m])
-        //     e = m-1;
-        //     else
-        //     piv = m+1;
-        // }
-        while(s<=e){
-            int mid = s + (e-s)/2;
-            if(nums[mid] == target)return mid;
-            if(nums[s] <=nums[mid]){
-                if(nums[s]<=target && target <= nums[mid])
-                e = mid-1;
+        int l=0,h=nums.size()-1;
+        while(l<=h){
+            int m = l + (h-l)/2;
+            if(nums[m] == target)return m;
+            if(nums[l]<=nums[m]){
+                if(nums[l]<=target && target <= nums[m])h = m-1;
                 else
-                s = mid+1;
-
+                l = m+1;
             }
             else{
-                if(nums[mid]<=target && target <= nums[e])
-                s = mid+1;
+                if(nums[h]>=target && target >= nums[m])l=m+1;
                 else
-                e = mid-1;
+                h=m-1;
             }
+
         }
         return -1;
     }
