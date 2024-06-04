edbdfb534f9ab57ce1da2165168ba0ea7462fce1
Olaf51 <112055781+Olaf51@users.noreply.github.com>
2024-06-04 09:49:17 +0530

    Time: 24 ms (45.49%) | Memory: 10.8 MB (39.33%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.cpp
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -1,32 +1,29 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        int n = s.size();
-        if(t.size()>n)return "";
         unordered_map<char,int>mp;
-        for(auto &ch: t)
-        mp[ch]++;
-        int i=0,j=0,start_i=0;
-        int widSize = INT_MAX, curCnt = t.size();
-        while(j<n){
+        for(auto &it: t)mp[it]++;
+        int cnt=0,r=0,l=0,n=s.size(),mx = INT_MAX,start=-1;
+        while(r<n){
+           
+            if(mp[s[r]]>0)cnt++;
+             mp[s[r]]--;
 
-            if(mp[s[j]]>0)
-            curCnt --;
-            mp[s[j]]--;
-            while(curCnt == 0){
-                int currSize = j-i+1;
-                if(widSize > currSize){
-                    widSize = currSize;
-                    start_i = i;
+            while(cnt == t.size()){
+                int m = r-l+1;
+                if(m<mx){
+                    mx = m;
+                    start = l;
                 }
-                mp[s[i]]++;
-                if(mp[s[i]]>0)
-                curCnt++;
-                i++;
+                mp[s[l]]++;
+                if(mp[s[l]]>0)cnt--;
+                l++;
 
             }
-            j++;
+            r++;
         }
-        return widSize == INT_MAX ? "":  s.substr(start_i,widSize);
+        return mx == INT_MAX ? "": s.substr(start,mx);
+
+        
     }
 };
