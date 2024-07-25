527c9883a7e5365ee8689eae28b2b504fad29382
Olaf51 <112055781+Olaf51@users.noreply.github.com>
2024-07-25 20:01:39 +0530

    Time: 71 ms (68.24%) | Memory: 22.9 MB (65.77%) - LeetSync

--- 1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.cpp
+++ 1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.cpp
@@ -1,19 +1,27 @@
 class Solution {
 public:
     string chek(string s, string m){
-        stack<char>st;
-        for(int i=0;i<s.size();i++){
-            if(s[i] == m[1] && !st.empty() && st.top() == m[0])st.pop();
-            else
-            st.push(s[i]);
+        // stack<char>st;
+        // for(int i=0;i<s.size();i++){
+        //     if(s[i] == m[1] && !st.empty() && st.top() == m[0])st.pop();
+        //     else
+        //     st.push(s[i]);
+        // }
+        // string temp;
+        // while(!st.empty()){
+        //     temp.push_back(st.top());
+        //     st.pop();
+        // }
+        // reverse(temp.begin(),temp.end());
+        // return temp;
+        int i=0;
+        for(int j=0;j<s.size();j++){
+            s[i++] = s[j];
+            if(i>=2 && s[i-2]==m[0] && s[i-1]==m[1])
+            i -=2;
         }
-        string temp;
-        while(!st.empty()){
-            temp.push_back(st.top());
-            st.pop();
-        }
-        reverse(temp.begin(),temp.end());
-        return temp;
+        s.erase(begin(s)+i, end(s));
+        return s;
     }
     int maximumGain(string s, int x, int y) {
         int n = s.size();
