0c2e904d81d5e032286f2988ad344dc85a243fb8
Olaf51 <112055781+Olaf51@users.noreply.github.com>
2024-07-25 19:57:36 +0530

    Time: 122 ms (17.21%) | Memory: 34.9 MB (7.30%) - LeetSync

--- 1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.cpp
+++ 1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.cpp
@@ -1,34 +1,31 @@
 class Solution {
 public:
-    int maximumGain(string s, int x, int y) {
-        int aCount = 0;
-        int bCount = 0;
-        int lesser = min(x, y);
-        int result = 0;
-
-        for (char c : s) {
-            if (c > 'b') {
-                result += min(aCount, bCount) * lesser;
-                aCount = 0;
-                bCount = 0;
-            } else if (c == 'a') {
-                if (x < y && bCount > 0) {
-                    bCount--;
-                    result += y;
-                } else {
-                    aCount++;
-                }
-            } else {
-                if (x > y && aCount > 0) {
-                    aCount--;
-                    result += x;
-                } else {
-                    bCount++;
-                }
-            }
+    string chek(string s, string m){
+        stack<char>st;
+        for(int i=0;i<s.size();i++){
+            if(s[i] == m[1] && !st.empty() && st.top() == m[0])st.pop();
+            else
+            st.push(s[i]);
         }
+        string temp;
+        while(!st.empty()){
+            temp.push_back(st.top());
+            st.pop();
+        }
+        reverse(temp.begin(),temp.end());
+        return temp;
+    }
+    int maximumGain(string s, int x, int y) {
+        int n = s.size();
+        string mx = (x>y)? "ab":"ba";
+        string mi = (x<y)? "ab":"ba";
+        string temp = chek(s,mx);
+        int cnt = n - temp.size();
+        int ans = (cnt/2)*max(x,y);
+        string temp_2 = chek(temp,mi);
+        ans += ((temp.size()-temp_2.size())/2)*min(x,y);
+        return ans;
 
-        result += min(aCount, bCount) * lesser;
-        return result;
+        
     }
 };
