7f358880b2f5dbbe899f6abcbb201a6a293d50de
Olaf51 <112055781+Olaf51@users.noreply.github.com>
2024-08-27 16:48:38 +0530

    Time: 568 ms (57.26%) | Memory: 0B (100.00%) - LeetSync

--- 1182-game-play-analysis-iv/game-play-analysis-iv.sql
+++ 1182-game-play-analysis-iv/game-play-analysis-iv.sql
@@ -4,10 +4,16 @@
 
 
 
-select round(count(distinct player_id)/(select count(distinct player_id) from Activity),2)
-as fraction 
-from Activity
-where (player_id, date_sub(event_date, INTERVAL 1 DAY)) in
-(select player_id, min(event_date) as first
-from Activity 
-group by player_id);
+-- select round(count(distinct player_id)/(select count(distinct player_id) from Activity),2)
+-- as fraction 
+-- from Activity
+-- where (player_id, date_sub(event_date, INTERVAL 1 DAY)) in
+-- (select player_id, min(event_date) as first
+-- from Activity 
+-- group by player_id);
+select round(sum(temp)/count(distinct player_id),2) as fraction from
+(select
+    player_id,
+    datediff(event_date, min(event_date) over(partition by player_id)) = 1 as temp
+  from 
+    Activity) as t
