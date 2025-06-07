fbaff0ae48b5b71e7bf6bcf1dc15938c486ffde6
Olaf51 <112055781+Olaf51@users.noreply.github.com>
2025-06-08 00:14:14 +0530

    Time: 406 ms (16.23%) | Memory: 106.8 MB (18.50%) - LeetSync

--- 3445-lexicographically-minimum-string-after-removing-stars/lexicographically-minimum-string-after-removing-stars.cpp
+++ 3445-lexicographically-minimum-string-after-removing-stars/lexicographically-minimum-string-after-removing-stars.cpp
@@ -1,34 +1,21 @@
 class Solution {
 public:
- struct comp{
-    bool operator()(pair<char,int>&p1,pair<char,int>&p2){
-        if(p1.first == p2.first)
-        return p1.second < p2.second;
-        return p1.first>p2.first;
-    }
- };
     string clearStars(string s) {
-        priority_queue<pair<char,int>,vector<pair<char,int>>,comp>pq;
-        int n = s.size();
-       
-        for(int i=0;i<n;i++){
-            if(s[i]== '*'){
-              
-                int id = pq.top().second;
-                pq.pop();
-                s[id] = '*';
+    priority_queue<pair<char,int>, vector<pair<char,int>>, greater<pair<char,int>>> minh;
+    unordered_map<int,bool> mpp;
+        for (int i = 0; i < s.size(); i++) {
+            if(s[i] == '*') {
+                mpp[-minh.top().second] = true;
+                minh.pop();
+            } else {
+                minh.push({s[i], -i});
             }
-            else
-            pq.push({s[i],i});
         }
-       
-       
-        string ans="";
-        for(int i=0;i<n;i++){
-            if(s[i]=='*')continue;
-            ans += s[i];
+        string res;
+        for(int i = 0; i < s.size(); i++) {
+            if(mpp.count(i) || s[i] == '*') continue;
+            res += s[i];
         }
-        return ans;
-
+        return res;
     }
 };
