e178f364fb03e75e4f1f18acf3c7f1cc2e896695
Olaf51 <112055781+Olaf51@users.noreply.github.com>
2025-06-18 23:04:47 +0530

    Time: 82 ms (20.10%) | Memory: 122.4 MB (24.64%) - LeetSync

--- 3241-divide-array-into-arrays-with-max-difference/divide-array-into-arrays-with-max-difference.cpp
+++ 3241-divide-array-into-arrays-with-max-difference/divide-array-into-arrays-with-max-difference.cpp
@@ -1,20 +1,22 @@
 class Solution {
 public:
     vector<vector<int>> divideArray(vector<int>& nums, int k) {
-        vector<vector<int>>ans;
-        int size = nums.size();
-       sort(begin(nums),end(nums));
-      vector<vector<int>> result(size / 3, vector<int>(3));
-        int groupIndex = 0;
-        for (int i = 0; i < size; i += 3) {
-            if (i + 2 < size && nums[i + 2] - nums[i] <= k) {
-                result[groupIndex] = { nums[i], nums[i + 1], nums[i + 2] };
-                groupIndex++;
-            }
+        // min-heap, putting elements in heap
+        priority_queue<int, vector<int>, greater<int>> pq(nums.begin(), nums.end());
+        vector<vector<int>> res;
+
+        while (pq.size() >= 3) {
+            int low = pq.top(); pq.pop();
+            int mid = pq.top(); pq.pop();
+            int great = pq.top(); pq.pop();
+
+            if (great - low <= k) {
+                res.push_back({low, mid, great});
+            } 
             else {
-                return vector<vector<int>>();
+                return {}; // Cannot group
             }
         }
-        return result;
+        return res;
     }
 };
